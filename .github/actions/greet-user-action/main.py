00012c2bb5c4a72e7088909124a58b0b2067710b
MilanLanghendries <milan.langhendries@student.ehb.be>
2024-11-04 17:53:29 +0100

    fix dockerfile

--- .github/actions/greet-user-action/main.py
+++ .github/actions/greet-user-action/main.py
@@ -1,8 +1,4 @@
 import os
 
-def main():
-    name = os.getenv('INPUT_NAME', 'GitHub User')
-    print(f"Hello, {name}! Welcome to the repository!")
-
-if __name__ == "__main__":
-    main()
+name = os.environ.get('INPUT_NAME', 'GitHub User')
+print(f'Hello, {name}!')
