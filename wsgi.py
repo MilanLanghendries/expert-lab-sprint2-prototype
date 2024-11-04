1fe8084a54251c2eba560f942c1550b9a65ade5a
MilanLanghendries <milan.langhendries@student.ehb.be>
2024-11-04 17:09:38 +0100

    switched to waitress for deploy

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
-from app import app  # Import the Flask app instance
+from app import create_app  # Import the create_app function
+
+app = create_app()  # Create an instance of the app
 
 if __name__ == "__main__":
-    app.run()
+    app.run()  # This is for local testing (not needed when using Waitress)
