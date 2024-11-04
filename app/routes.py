e544cbafa674432cc09970a5bab533d9cfca1a08
MilanLanghendries <milan.langhendries@student.ehb.be>
2024-11-04 15:13:18 +0100

    Fix template loading issue by moving templates folder inside app directory

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html")  
 
 @main.route("/api/message")
 def random_message():
